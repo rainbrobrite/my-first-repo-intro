caa96c30c130e01925e946086fe1c52c4d7aa875
JJ Javelet <jetjavelet@gmail.com>
2023-11-13 18:23:29 -0800

    yes

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".page-title").hide().fadeIn(2000);
+    $(".backContent").hide().fadeIn(4000);
+    $(".menu-selection").hide().delay(6000).fadeIn(2000);
+    $(".Favorites").hide().delay(10000).fadeIn(2000);
+    $(".food").hide().delay(12000).fadeIn(2000);
+    $(".hoursContent").hide().delay(14000).fadeIn(2000);
+  });
+  
